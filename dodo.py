0cef979407348a847912ca08d2410fb24f425d01
Chris B <ceball@gmail.com>
2018-07-12 13:25:24 +0100

    Fixups, and use pyctdev for more.

--- dodo.py
+++ dodo.py
@@ -3,15 +3,3 @@ if "PYCTDEV_ECOSYSTEM" not in os.environ:
     os.environ["PYCTDEV_ECOSYSTEM"] = "conda"
 
 from pyctdev import *  # noqa: api
-
-
-############################################################
-# Website building tasks; will move out to pyct
-
-def task_docs():
-    return {'actions': [
-        'nbsite_nbpagebuild.py ioam parambokeh ./examples ./doc',
-        'sphinx-build -b html ./doc ./doc/_build/html',
-        'nbsite_fix_links.py ./doc/_build/html',
-        'touch ./doc/_build/html/.nojekyll',
-        'nbsite_cleandisthtml.py ./doc/_build/html take_a_chance']}
